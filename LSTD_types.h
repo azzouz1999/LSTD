486aa4ecf09e5c2b64cb8e2a1526e0bb690d437a
azzouz1999 <148722696+azzouz1999@users.noreply.github.com>
2023-12-31 22:09:40 +0200

    Add files via upload

---
+++ LSTD_types.h
@@ -0,0 +1,25 @@
+/************************************/
+/* Author		: Ali Azzouz		*/
+/* SWC			: Standard Types	*/
+/* Layer		: Library			*/
+/* Version		: 1.0				*/
+/* Date			: December 1, 2023	*/
+/* Last Edit	: N/A				*/
+/************************************/
+
+#ifndef _LSTD_TYPES_H_
+#define _LSTD_TYPES_H_
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+typedef signed short int s16;
+typedef signed long int s32;
+
+typedef float f32;
+typedef double f64;
+
+#endif /*_LSTD_TYPES_H_*/
